63d915f3e18276e69e871347e8452b06d5fcdcdd
Vansh Patel <patel.vansh1607@gmail.com>
2025-04-08 14:38:37 +0300

    added

--- src/components/calculator.js
+++ src/components/calculator.js
@@ -1,47 +1,61 @@
-import "../styles/calculator.css"
-
+import React, { useState } from "react";
+import "../styles/calculator.css";
 
 const Calculator = () => {
-    return(
-    <div className="container">
-        <div className="calculator">
-            <form action="">
-                <div  className="display">
-                    <input type="text"/>
-                    <div>
-                        <input type="button" value="AC"/>
-                        <input type="button" value="DE"/>
-                        <input type="button" value="."/>
-                        <input type="button" value="/"/>
+    const [value, setValue] = useState("");
+
+    const handleClick = (e) => {
+        setValue(value + e.target.value);
+    };
+
+    const handleClear = () => {
+        setValue("");
+    };
+
+    const handleDelete = () => {
+        setValue(value.slice(0, -1));
+    };
+
+    const handleCalculate = () => {
+        try {
+            setValue(eval(value).toString());
+        } catch (error) {
+            setValue("Error");
+        }
+    };
+
+    return (
+        <div className="container">
+            <div className="calculator">
+                <form>
+                    <div className="display">
+                        <input type="text" value={value} readOnly />
                     </div>
-                    <div>
-                        <input type="button" value="7"/>
-                        <input type="button" value="8"/>
-                        <input type="button" value="9"/>
-                        <input type="button" value="*"/>
+                    <div className="buttons">
+                        <input type="button" value="AC" onClick={handleClear} />
+                        <input type="button" value="DE" onClick={handleDelete} />
+                        <input type="button" value="." onClick={handleClick} />
+                        <input type="button" value="/" onClick={handleClick} />
+                        <input type="button" value="7" onClick={handleClick} />
+                        <input type="button" value="8" onClick={handleClick} />
+                        <input type="button" value="9" onClick={handleClick} />
+                        <input type="button" value="*" onClick={handleClick} />
+                        <input type="button" value="4" onClick={handleClick} />
+                        <input type="button" value="5" onClick={handleClick} />
+                        <input type="button" value="6" onClick={handleClick} />
+                        <input type="button" value="+" onClick={handleClick} />
+                        <input type="button" value="1" onClick={handleClick} />
+                        <input type="button" value="2" onClick={handleClick} />
+                        <input type="button" value="3" onClick={handleClick} />
+                        <input type="button" value="-" onClick={handleClick} />
+                        <input type="button" value="00" onClick={handleClick} />
+                        <input type="button" value="0" onClick={handleClick} />
+                        <input type="button" value="=" className="equal" onClick={handleCalculate} />
                     </div>
-                    <div>
-                        <input type="button" value="4"/>
-                        <input type="button" value="5"/>
-                        <input type="button" value="6"/>
-                        <input type="button" value="+"/>
-                    </div>
-                    <div>
-                        <input type="button" value="1"/>
-                        <input type="button" value="2"/>
-                        <input type="button" value="3"/>
-                        <input type="button" value="-"/>
-                    </div>
-                    <div>
-                        <input type="button" value="00"/>
-                        <input type="button" value="0"/>
-                        <input type="button" value="="/>
-                    </div>
-                </div>
-            </form>
+                </form>
+            </div>
         </div>
-    </div>
-    )
-}
+    );
+};
 
 export default Calculator;
