19439d664ee8afa255e7d70a942080b6d6e0b73f
Vansh Patel <patel.vansh1607@gmail.com>
2025-04-08 14:17:34 +0300

    feat: add Calculator component with basic input field

---
+++ src/components/calculator.js
@@ -0,0 +1,18 @@
+import "../styles/calculator.css"
+
+
+const Calculator = () => {
+    return(
+    <div className="container">
+        <div className="calculator">
+            <form action="">
+                <div>
+                    <input type="text"/>
+                </div>
+            </form>
+        </div>
+    </div>
+    )
+}
+
+export default Calculator;
